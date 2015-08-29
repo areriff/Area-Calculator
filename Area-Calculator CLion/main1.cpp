852a6c7e3b373862c1e897b40cd23ae1f5814184
areriff <arif_nfbmm@hotmail.com>
2015-08-30 01:27:12 +0800

    CLion First Commit

---
+++ Area-Calculator CLion/main1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arif_ on 8/30/2015.
+//
+
+#include "main1.h"
---
+++ Area-Calculator CLion/main1.h
@@ -0,0 +1,8 @@
+//
+// Created by arif_ on 8/30/2015.
+//
+
+#ifndef AREA_CALCULATOR_MAIN1_H
+#define AREA_CALCULATOR_MAIN1_H
+
+#endif //AREA_CALCULATOR_MAIN1_H
